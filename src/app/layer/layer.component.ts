498c7261a67d25f8cc22dfccb687ef7fa888c3a2
Lisa Anthonioz <lisa.anthonioz@isen.yncrea.fr>
2018-03-01 10:47:36 +0100

    Layer fonctionel

--- src/app/layer/layer.component.ts
+++ src/app/layer/layer.component.ts
@@ -14,25 +14,35 @@ export class LayerComponent implements OnInit {
   coordGare = L.latLng(43.1283184, 5.9272719);
 
   ngOnInit() {
-    const mapboxUrl = "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
-      mapboxAttribution = "My Map";
-
-    const littleton = L.marker([39.61, -105.02]).bindPopup(
-        "This is Littleton, CO."
+    const iconMarker = L.icon({
+      iconUrl:
+        "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-icon.png"
+    });
+    const littleton = L.marker([39.61, -105.02], {
+        icon: iconMarker
+      }).bindPopup("This is Littleton, CO."),
+      denver = L.marker([39.74, -104.99], { icon: iconMarker }).bindPopup(
+        "This is Denver, CO."
       ),
-      denver = L.marker([39.74, -104.99]).bindPopup("This is Denver, CO."),
-      aurora = L.marker([39.73, -104.8]).bindPopup("This is Aurora, CO."),
-      golden = L.marker([39.77, -105.23]).bindPopup("This is Golden, CO.");
+      aurora = L.marker([39.73, -104.8], { icon: iconMarker }).bindPopup(
+        "This is Aurora, CO."
+      ),
+      golden = L.marker([39.77, -105.23], { icon: iconMarker }).bindPopup(
+        "This is Golden, CO."
+      );
 
     const cities = L.layerGroup([littleton, denver, aurora, golden]);
 
-    const grayscale = L.tileLayer(mapboxUrl, {
-        id: "MapID",
-        attribution: mapboxAttribution
-      }),
-      streets = L.tileLayer(mapboxUrl, {
-        id: "MapID",
-        attribution: mapboxAttribution
+    const grayscale = L.tileLayer(
+        "http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png",
+        {
+          attribution:
+            // tslint:disable-next-line:quotemark
+            '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }
+      ),
+      streets = L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+        attribution: "My Map"
       });
 
     const map = L.map("frugalmap", {
@@ -55,3 +65,4 @@ export class LayerComponent implements OnInit {
 }
 
 // doc : http://leafletjs.com/examples/layers-control/
+// Pour changer le style de la map : Leaflet Providers -> http://leaflet-extras.github.io/leaflet-providers/preview/
