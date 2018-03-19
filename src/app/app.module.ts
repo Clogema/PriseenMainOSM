f63387474525e02811e08e297d432193f484abf9
Lisa Anthonioz <lisa.anthonioz@isen.yncrea.fr>
2018-03-19 11:54:38 +0100

    Essai de Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,27 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
 import { LayerComponent } from "./layer/layer.component";
 import { QuickStartComponent } from "./quick-start/quick-start.component";
+import { LoginFormComponent } from "./login-form/login-form.component";
 
 const appRoutes: Routes = [
   { path: "layer", component: LayerComponent },
-  { path: "quick", component: QuickStartComponent }
+  { path: "quick", component: QuickStartComponent },
+  { path: "login", component: LoginFormComponent }
 ];
 
 @NgModule({
-  declarations: [AppComponent, LayerComponent, QuickStartComponent],
-  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
+  declarations: [
+    AppComponent,
+    LayerComponent,
+    QuickStartComponent,
+    LoginFormComponent
+  ],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes), HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
